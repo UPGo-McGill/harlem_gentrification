44badc500381d0aee2e7c0b30e48da2a9f4a5e76
rbbinder <robin.basalaev-binder@mail.mcgill.ca>
2021-06-29 21:18:19 -0400

    basic 2009 vs 2019 maps

--- R/02_data_import.R
+++ R/02_data_import.R
@@ -3,7 +3,8 @@
 # Load libraries and helper functions -------------------------------------
 
 source("R/01_helper_functions.R")
-
+library(tidyverse)
+install.packages("tidyverse")
 
 # Import census geographies -----------------------------------------------
 
@@ -26,12 +27,13 @@ nyc_city <- suppressWarnings(
       st_transform(26918) %>% 
       st_union())
 
+tigris::w
 
 # Import census data ------------------------------------------------------
 
-### 2018 ACS
+### 2019 ACS
 
-CTs_2018 <- 
+CTs_2019 <- 
   get_acs(
   geography = "tract", 
   variables = c(med_income = "B19013_001",
@@ -39,7 +41,7 @@ CTs_2018 <-
                 pop_hisp = "B03002_012",
                 pop_black = "B03002_004",
                 immigrant = "B05001_006"),
-  year = 2018, 
+  year = 2019, 
   state = "36",
   county = c("New York County",
              "Kings County",
@@ -59,50 +61,57 @@ CTs_2018 <-
   na.omit() %>% 
   select(-geometry, everything(), geometry)
 
-### 2000 decennial
+### 2009 ACS
 
-CTs_2000 <- get_decennial(
-  geography = "tract", 
-  variables = c(#med_income = "P053001",
-                pop_white = "P007003",
-                pop_hisp = "P007010",
-                pop_black = "P007004",
-                immigrant = "P021015"),
-  year = 2000, 
-  state = "36",
-  county = c("New York County",
-             "Kings County",
-             "Queens County",
-             "Bronx County",
-             "Richmond County"),
-  summary_var = "P001001",
-  geometry = TRUE) %>% 
+CTs_2009 <- 
+  get_acs(
+    geography = "tract", 
+    variables = c(med_income = "B19013_001",
+                  pop_white = "B03002_003",
+                  pop_hisp = "B03002_012",
+                  pop_black = "B03002_004",
+                  immigrant = "B05001_006"),
+    year = 2009, 
+    state = "36",
+    county = c("New York County",
+               "Kings County",
+               "Queens County",
+               "Bronx County",
+               "Richmond County"),
+    summary_var = "B01003_001",
+    geometry = TRUE) %>% 
+  as_tibble() %>%
+  st_as_sf() %>% 
   st_transform(26918) %>% 
-  rename(estimate = value, pop_total = summary_value) %>% 
+  rename(pop_total = summary_est) %>% 
+  select(-moe, -summary_moe) %>% 
   spread(key = variable, value = estimate) %>% 
   mutate(pop_density = pop_total/st_area(geometry)) %>% 
   # filter(pop_total > 100) %>%
   na.omit() %>% 
   select(-geometry, everything(), geometry)
 
+
 # Add percentage variables
 
-CTs_2015 <- 
-  CTs_2015 %>% 
+CTs_2009 <- 
+  CTs_2009 %>% 
   mutate_at(c("pop_white", "immigrant", "pop_black", "pop_hisp"),
-            list(pct = ~. / CTs_2015$pop_total))
+            list(pct = ~. / CTs_2009$pop_total))
 
-CTs_2000 <- 
-  CTs_2000 %>% 
+CTs_2019 <- 
+  CTs_2019 %>% 
   mutate_at(c("pop_white", "immigrant", "pop_black", "pop_hisp"),
-            list(pct = ~. / CTs_2000$pop_total)) # %>% 
+            list(pct = ~. / CTs_2019$pop_total)) # %>% 
+
+
 #  filter(GEOID != "36061024000")
 
 
 ## Clip data to water
 
-CTs_2000 <- st_erase(CTs_2000, nyc_water)
-CTs_2015 <- st_erase(CTs_2015, nyc_water)
+# CTs_2009 <- st_erase(CTs_2009, nyc_water)
+# CTs_2019 <- st_erase(CTs_2019, nyc_water)
 
 # Get East Harlem bounding box
 
@@ -122,6 +131,46 @@ harlem <- nyc_pumas %>%
   st_erase(nyc_water) %>% 
   st_cast("POLYGON") 
  
+harlem_
+
+
 harlem <- harlem[1,]
 
 plot(harlem)
+
+# Add year to data, then combine 2000 and 2015
+
+CTs_2019 <- 
+  CTs_2019 %>% 
+  mutate("Year" = 2019)
+
+CTs_2009 <- 
+  CTs_2009 %>% 
+  mutate("Year" = 2009)
+
+CTs_2009_2019 <- 
+  rbind(CTs_2009, CTs_2019)
+
+# Get East Harlem CTs
+
+CT_harlem <- 
+  CTs_2009_2019 %>% 
+  st_centroid() %>% 
+  st_filter(harlem) #%>% 
+  st_drop_geometry() %>% 
+  select(GEOID)
+
+
+ggplot() + 
+  # st_as_sf() %>% 
+  geom_sf(data = CTs_2009_2019, fill = "blue", colour = "white", 
+          lwd = 0.05) +
+  facet_wrap(~ Year) +
+  geom_sf(data = CT_harlem, fill = "red", color = "black")+
+  theme_void() +
+  coord_sf(xlim = st_bbox(harlem) [c(1,3)] * c(0.9998, 1.00039), 
+           ylim = st_bbox(harlem) [c(2,4)] * c(0.999999, 1.0001))
+
+plot(harlem)
+
+
--- R/05_mapping.R
+++ R/05_mapping.R
@@ -13,19 +13,232 @@ gg_bbox <- function(geom, x1 = 0, x2 = 1, y1 = 0, y2 = 1) {
     c(1 - y1, y1, 1- y2, y2), nrow = 2)
   
   coord_sf(xlim = as.vector(matrix_x), ylim = as.vector(matrix_y))
+}
 
-# Add year to data, then combine 2000 and 2015
 
-CTs_2015 <- 
-  CTs_2015 %>% 
-  mutate("Year" = 2015)
 
-CTs_fixed_2000 <- 
-  CTs_fixed_2000 %>% 
-  mutate("Year" = 2000)
+# median income
 
-CTs_2000_2015 <- 
-  rbind(CTs_fixed_2000, CTs_2015)
+MHI_map <- 
+  ggplot() + 
+  # st_as_sf() %>% 
+  geom_sf(data = CTs_2009_2019, aes(fill = med_income), colour = "white", 
+          lwd = 0.05) +
+  scale_fill_viridis_b(breaks = c(10000, 25000, 40000, 55000, 70000), 
+                        limits = c(0,100000), name = "Median Income", 
+                       option = "E") +
+  facet_wrap(~ Year) +
+  geom_sf() + 
+  geom_sf(data = nyc_water, colour = "white", fill = "white") +
+  ggtitle("Median Household Income")+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  coord_sf(xlim = st_bbox(harlem) [c(1,3)] * c(0.9998, 1.00039), 
+          ylim = st_bbox(harlem) [c(2,4)] * c(0.999999, 1.0001))
+
+# percent Hispanic
+
+hisp_map <- 
+  ggplot() + 
+  # st_as_sf() %>% 
+  geom_sf(data = CTs_2009_2019, aes(fill = pop_hisp_pct), colour = "white", 
+          lwd = 0.05) +
+  scale_fill_viridis_b(name = "% Hispanic", option = "E") +
+  facet_wrap(~ Year) +
+  geom_sf() + 
+  geom_sf(data = nyc_water, colour = "white", fill = "white") +
+  ggtitle("Percent Hispanic population")+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  coord_sf(xlim = st_bbox(harlem) [c(1,3)] * c(0.9998, 1.00039), 
+           ylim = st_bbox(harlem) [c(2,4)] * c(0.999999, 1.0001))
+
+
+# percent Black
+
+pct_black_map <- 
+  ggplot() + 
+  # st_as_sf() %>% 
+  geom_sf(data = CTs_2009_2019, aes(fill = pop_black_pct), colour = "white", 
+          lwd = 0.05) +
+  scale_fill_viridis_b(name = "% Immigrant", option = "E") +
+  facet_wrap(~ Year) +
+  geom_sf() + 
+  geom_sf(data = nyc_water, colour = "white", fill = "white") +
+  ggtitle("Percent Black population")+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  coord_sf(xlim = st_bbox(harlem) [c(1,3)] * c(0.9998, 1.00039), 
+           ylim = st_bbox(harlem) [c(2,4)] * c(0.999999, 1.0001))
+
+
+# immigrant percent
+
+pct_imm_map <- 
+  ggplot() + 
+  # st_as_sf() %>% 
+  geom_sf(data = CTs_2009_2019, aes(fill = immigrant_pct), colour = "white", 
+          lwd = 0.05) +
+  scale_fill_viridis_b(option = "E") +
+  facet_wrap(~ Year) +
+  geom_sf() + 
+  geom_sf(data = nyc_water, colour = "white", fill = "white") +
+  ggtitle("Percent immigrant population")+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  coord_sf(xlim = st_bbox(harlem) [c(1,3)] * c(0.9998, 1.00039), 
+           ylim = st_bbox(harlem) [c(2,4)] * c(0.999999, 1.0001))
+
+
+# percent non Hispanic white
+
+pct_white_map <- 
+  ggplot() + 
+  # st_as_sf() %>% 
+  geom_sf(data = CTs_2009_2019, aes(fill = pop_white_pct), colour = "white", 
+          lwd = 0.05) +
+  scale_fill_viridis_b(name = "% Non-Hispanic white", breaks = c(0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3), 
+                       limits = c(0,0.5), option = "E") +
+  facet_wrap(~ Year) +
+  geom_sf() + 
+  geom_sf(data = nyc_water, colour = "white", fill = "white") +
+  ggtitle("Percent white population")+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  coord_sf(xlim = st_bbox(harlem) [c(1,3)] * c(0.9998, 1.00039), 
+           ylim = st_bbox(harlem) [c(2,4)] * c(0.999999, 1.0001))
+
+ggsave("output/figure_1.png", plot = MHI_map)
+ggsave("output/figure_2.png", plot = hisp_map)
+ggsave("output/figure_3.png", plot = pct_black_map)
+ggsave("output/figure_4.png", plot = pct_imm_map)
+ggsave("output/figure_5.png", plot = pct_white_map)
+
+
+## Mapping 2009 and 2019
+
+CTs_2009_2019 %>% 
+  ggplot() +
+  geom_sf(aes(fill = med_income)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_x_continuous(breaks = c(20000, 40000, 60000)) +
+  scale_fill_continuous(breaks = c(10000, 25000, 40000, 55000, 70000), limits = c(0,100000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) %>% 
+  facet_grid("Year")
+
+CTs_2009 %>% 
+  ggplot() +
+  geom_sf(aes(fill = pop_hisp_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2019 %>% 
+  ggplot() +
+  geom_sf(aes(fill = pop_hisp_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2009 %>% 
+  ggplot() +
+  geom_sf(aes(fill = pop_black_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2019 %>% 
+  ggplot() +
+  geom_sf(aes(fill = pop_black_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2009 %>% 
+  ggplot() +
+  geom_sf(aes(fill = pop_white_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2019 %>% 
+  ggplot() +
+  geom_sf(aes(fill = pop_white_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2009 %>% 
+  ggplot() +
+  geom_sf(aes(fill = immigrant_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+CTs_2019 %>% 
+  ggplot() +
+  geom_sf(aes(fill = immigrant_pct)) +
+  # scale_fill_gradientn(limits = c(0,1000), colors = c("#9DBF9E", "#FCB97D", "#A84268"),
+  #                      na.value = "grey80") +
+  #scale_fill_continuous(breaks = c(20000, 35000, 50000, 65000, 80000)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "gray"),
+    panel.grid.major = element_line(colour = "gray")) +
+  gg_bbox(harlem) 
+
+
+
 
 # Standardize and map data
 
@@ -53,6 +266,7 @@ CTs_standard <-
 
 
 
+
 ## Tidy data for ggplot mapping
 
 #CTs_mapping <- 
@@ -71,6 +285,9 @@ gg_bbox(harlem)
 
 
 }
+
+
+
 
 
 ## Test for NAs 
